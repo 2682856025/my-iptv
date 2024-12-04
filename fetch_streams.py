4fd98f4b8d070facb7dcd21452fb0fd2c549fbe2
爱吃蛋炒饭 <106012957+2682856025@users.noreply.github.com>
2024-12-04 08:57:41 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -13,6 +13,7 @@ urls = [
     "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u",
     'https://raw.githubusercontent.com/BurningC4/Chinese-IPTV/master/TV-IPV4.m3u',
     "https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/IPV6.m3u",
+    "https://live.zbds.top/tv/iptv6.txt",
 ]
 
 # 区分IPv4和IPv6的正则表达式
